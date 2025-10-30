fa01b40eaffa00fd4fa7a93556a6b10a87c7a1d7
samirr2502 <samirrodriguez2502@gmail.com>
2025-10-30 12:30:56 -0600

    renamed basename

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/portfolio">
+    <BrowserRouter basename="/">
     <App />
     </BrowserRouter>
   </StrictMode>,
